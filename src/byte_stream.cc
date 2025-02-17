0bf05b9dcdff051ff228251d5902a5b7779449ed
yali-hzy <yali-hzy@qq.com>
2025-02-17 18:18:24 +0800

    feat: byte_stream with std:string buffer_

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -6,51 +6,56 @@ ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 
 void Writer::push( string data )
 {
-  (void)data; // Your code here.
+  if ( data.size() + buffer_.size() > capacity_ ) {
+    data.resize( capacity_ - buffer_.size() );
+  }
+  buffer_ += data;
+  bytes_pushed_ += data.size();
 }
 
 void Writer::close()
 {
-  // Your code here.
+  closed_ = true;
 }
 
 bool Writer::is_closed() const
 {
-  return {}; // Your code here.
+  return closed_;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  return {}; // Your code here.
+  return capacity_ - buffer_.size();
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  return {}; // Your code here.
+  return bytes_pushed_;
 }
 
 string_view Reader::peek() const
 {
-  return {}; // Your code here.
+  return buffer_;
 }
 
 void Reader::pop( uint64_t len )
 {
-  (void)len; // Your code here.
+  bytes_popped_ += len;
+  buffer_.erase( 0, len );
 }
 
 bool Reader::is_finished() const
 {
-  return {}; // Your code here.
+  return closed_ && buffer_.empty();
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  return {}; // Your code here.
+  return buffer_.size();
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  return {}; // Your code here.
+  return bytes_popped_;
 }
 
